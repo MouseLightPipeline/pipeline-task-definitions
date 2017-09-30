6ad4f27d0ec211de58b6a91b3a89266e4700ddcc
Patrick Edson <pedson@users.noreply.github.com>
2017-09-30 09:42:39 -0400

    Verify default arguments as part of test script.

--- development/zcomp.sh
+++ development/zcomp.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+project_name=$1
+project_root=$2
+pipeline_input_root=$3
+pipeline_output_root=$4
+tile_relative_path=$5
+tile_name=$6
+log_root_path=$7
+z_minus_1_relative_path=$8
+z_minus_1_tile_name=$9
+expected_exit_code=${10}
+is_cluster_job=${11}
+
 echo $1
 echo $2
 echo $3
@@ -11,7 +23,6 @@ echo $8
 echo $9
 echo ${10}
 echo ${11}
-echo ${12}
 
 DUR=$(($RANDOM % 10 + 4))
 
