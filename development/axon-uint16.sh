47bec58acb28fa843ecb14a779819e9e33db630d
Patrick Edson <pedson@users.noreply.github.com>
2017-12-11 20:12:40 -0500

    Convert additional scripts to new cluster management.

---
+++ development/axon-uint16.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# Standard arguments passed to all tasks.
+project_name=$1
+project_root=$2
+pipeline_input_root=$3
+pipeline_output_root=$4
+tile_relative_path=$5
+tile_name=$6
+log_root_path=$7
+expected_exit_code=$8
+worker_id=$9
+is_cluster_job=${10}
+
+# Custom task arguments defined by task definition
+ilastik_project="${11}/axon_uint16.ilp"
+
+# Compile derivatives
+input_file1="$pipeline_input_root/$tile_relative_path/$tile_name-ngc.0.tif"
+input_file2="$pipeline_input_root/$tile_relative_path/$tile_name-ngc.1.tif"
+
+output_file="$pipeline_output_root/$tile_relative_path/$tile_name"
+output_file+="-prob"
+output_file_1="$output_file.0.h5"
+output_file_2="$output_file.1.h5"
+
+log_path_base="$pipeline_output_root/$tile_relative_path/.log"
+log_file_base="ax-${tile_name}"
+
+# Create hidden log folder
+mkdir -p ${log_path_base}
+
+# Make sure group can read/write.
+chmod ug+rwx ${log_path_base}
+chmod o+rx ${log_path_base}
+
+log_file_1="${log_path_base}/${log_file_base}-log.0.txt"
+log_file_2="${log_path_base}/${log_file_base}-log.1.txt"
+
+output_format="hdf5"
+
+# Default location on test and production machines.  Can also export IL_PREFIX in worker profile script (typically id.sh).
+if [ -z "$IL_PREFIX" ]
+then
+  if [ "$(uname)" == "Darwin" ]
+  then
+    IL_PREFIX=/Volumes/Spare/Projects/MouseLight/Classifier/ilastik/ilastik-1.1.8-OSX.app/Contents/ilastik-release
+  else
+    IL_PREFIX=/groups/mousebrainmicro/mousebrainmicro/cluster/software/ilastik-1.1.9-Linux
+  fi
+fi
+
+cmd1="${IL_PREFIX}/bin/python ${IL_PREFIX}/ilastik-meta/ilastik/ilastik.py --logfile=${log_file_1} --headless --cutout_subregion=\"[(None,None,None,0),(None,None,None,1)]\" --project=\"${ilastik_project}\" --output_filename_format=\"${output_file_1}\" --output_format=\"${output_format}\" \"$input_file1\""
+cmd2="${IL_PREFIX}/bin/python ${IL_PREFIX}/ilastik-meta/ilastik/ilastik.py --logfile=${log_file_2} --headless --cutout_subregion=\"[(None,None,None,0),(None,None,None,1)]\" --project=\"${ilastik_project}\" --output_filename_format=\"${output_file_2}\" --output_format=\"${output_format}\" \"$input_file2\""
+
+export LD_LIBRARY_PATH=""
+export PYTHONPATH=""
+export QT_PLUGIN_PATH=${IL_PREFIX}/plugins
+
+export LAZYFLOW_THREADS=18
+export LAZYFLOW_TOTAL_RAM_MB=200000
+
+# Channel 0
+eval ${cmd1}
+
+# Store before the next calls change the value.
+exit_code=$?
+
+if [ -e ${output_file_1} ]
+then
+    chmod 775 ${output_file_1}
+fi
+
+if [ -e ${log_file_1} ]
+then
+    chmod 775 ${log_file_1}
+fi
+
+if [ ${exit_code} -eq ${expected_exit_code} ]
+then
+  echo "Completed classifier for channel 0."
+else
+  echo "Failed classifier for channel 0."
+  exit ${exit_code}
+fi
+
+# Channel 1
+eval ${cmd2}
+
+exit_code=$?
+
+if [ -e ${output_file_2} ]
+then
+    chmod 775 ${output_file_2}
+fi
+
+if [ -e ${log_file_2} ]
+then
+    chmod 775 ${log_file_2}
+fi
+
+if [ ${exit_code} -eq ${expected_exit_code} ]
+then
+  echo "Completed classifier for channel 1."
+else
+  echo "Failed classifier for channel 1."
+fi
+
+exit ${exit_code}
---
+++ development/pointMatch.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Standard arguments passed to all tasks.
+project_name=$1
+project_root=$2
+pipeline_input_root=$3
+pipeline_output_root=$4
+tile_relative_path=$5
+tile_name=$6
+log_root_path=$7
+z_plus_1_relative_path=$8
+z_plus_1_tile_name=$9
+expected_exit_code=${10}
+worker_id=${11}
+is_cluster_job=${12}
+
+# Custom task arguments defined by task definition
+app="${13}/pointmatch"
+mcrRoot=${14}
+
+# Compile derivatives
+input_tile_1="${pipeline_input_root}/${tile_relative_path}"
+input_tile_2="${pipeline_input_root}/${z_plus_1_relative_path}"
+
+acq_folder_1="${project_root}/${tile_relative_path}"
+acq_folder_2="${project_root}/${z_plus_1_relative_path}"
+
+output_tile="${pipeline_output_root}/${tile_relative_path}"
+
+log_path_base="$pipeline_output_root/${tile_relative_path}/.log"
+log_file_base="pm-${tile_name}"
+
+# Create hidden log folder
+mkdir -p ${log_path_base}
+
+# Make sure group can read/write.
+chmod ug+rwx ${log_path_base}
+chmod o+rx ${log_path_base}
+
+log_file_1="${log_path_base}/${log_file_base}-log.0.txt"
+log_file_2="${log_path_base}/${log_file_base}-log.1.txt"
+
+# Various issues with this already existing in some accounts and not others, ssh conflicts to cluster depending on the
+# environment, etc.  Call it LD_LIBRARY_PATH2 for now and insert it as LD_LIBRARY_PATH at appropriate time.
+LD_LIBRARY_PATH2=.:${mcrRoot}/runtime/glnxa64 ;
+LD_LIBRARY_PATH2=${LD_LIBRARY_PATH2}:${mcrRoot}/bin/glnxa64 ;
+LD_LIBRARY_PATH2=${LD_LIBRARY_PATH2}:${mcrRoot}/sys/os/glnxa64;
+LD_LIBRARY_PATH2=${LD_LIBRARY_PATH2}:${mcrRoot}/sys/opengl/lib/glnxa64;
+
+cmd="${app} ${input_tile_1} ${input_tile_2} ${acq_folder_1} ${acq_folder_2} ${output_tile} ${expected_exit_code}"
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH2};
+
+export MCR_CACHE_ROOT="~/";
+
+eval ${cmd} &> ${log_file_1}
+
+# Store before the next calls change the value.
+exit_code=$?
+
+if [ -e ${log_file_1} ]
+then
+    chmod 775 ${log_file_1}
+fi
+
+if [ ${exit_code} -eq ${expected_exit_code} ]
+then
+  echo "Completed pointMatch 0."
+else
+  echo "Failed pointMatch 0."
+  exit ${exit_code}
+fi
