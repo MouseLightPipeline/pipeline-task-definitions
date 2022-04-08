37918440c544bff3391366c63f086cf602565612
Adam L. Taylor <adamlyletaylor@gmail.com>
2022-04-08 16:39:55 -0400

    Added more task definitions

---
+++ default/compute-z-matches-task.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Standard arguments passed to all tasks.
+pipeline_input_root=${1}
+pipeline_output_root=${2}
+tile_relative_path=${3}
+# Unused - tile_name=${4}
+fluorescence_root=${5}
+other_tile_relative_path=${6}
+mouselight_toolbox_path=${7}
+
+
+# User-defined arguments
+app="${mouselight_toolbox_path}//compute_z_matches.sh"
+#pythonFolder=${6}
+
+# Fallback exit code in case of error
+exit_code=255
+
+# Compile derivatives
+#input_file="${pipeline_input_root}/${tile_relative_path}"
+
+#cmd="${pythonFolder}/bin/python ${app} -i ${input_file} -o ${output_file}"
+cmd="${app} ${pipeline_input_root} ${pipeline_output_root} ${tile_relative_path} ${fluorescence_root} ${other_tile_relative_path}"
+echo "${cmd}"
+eval "${cmd}"
+
+# Store before the next calls change the value.
+exit_code=$?
+
+# Make the output file group-writable
+output_file="${pipeline_output_root}/${tile_relative_path}"
+if [ -e "${output_file}" ]
+then
+    chmod 775 "${output_file}"
+fi
+
+exit ${exit_code}
---
+++ default/fix-line-shift-task.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Standard arguments passed to all tasks.
+pipeline_input_root=${1}
+pipeline_output_root=${2}
+tile_relative_path=${3}
+# Unused - tile_name=${4}
+
+# User-defined arguments
+app="${5}/fix_line_shift.sh"
+#pythonFolder=${6}
+
+exit_code=255
+
+# Compile derivatives
+#input_file="${pipeline_input_root}/${tile_relative_path}"
+
+#cmd="${pythonFolder}/bin/python ${app} -i ${input_file} -o ${output_file}"
+cmd="${app} ${pipeline_input_root} ${tile_relative_path} ${pipeline_output_root}"
+echo "${cmd}"
+eval "${cmd}"
+
+# Store before the next calls change the value.
+exit_code=$?
+
+# Make the output file group-writable
+output_file="${pipeline_output_root}/${tile_relative_path}"
+if [ -e "${output_file}" ]
+then
+    chmod 775 "${output_file}"
+fi
+
+exit ${exit_code}
